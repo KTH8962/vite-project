e91dd8c10110a36c9f9a7d85f307d2d3272104aa
KTH8962 <kth8962@gmail.com>
2025-01-14 18:05:09 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { fileURLToPath, URL } from "url"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+    },
+  },
+  // SCSS 전역 사용
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/common.scss" as *;`,
+      },
+    },
+  },
+})
