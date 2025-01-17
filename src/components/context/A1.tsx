42c3f730a0b06900fd8c8b83ab44f8be817dba9a
KTH8962 <kth8962@gmail.com>
2025-01-17 22:06:42 +0900

    컨텍스트 소비

--- src/components/context/A1.tsx
+++ src/components/context/A1.tsx
@@ -1,9 +1,19 @@
+import { ColorContext, CountContext } from '@/contexts/colorContext'
 import A2 from './A2'
 
 function A1() {
   return (
     <>
-      <p>A1</p>
+      <ColorContext.Consumer>
+        {colorState => {
+          const [color] = colorState ?? colorState // !는 undefined나 null이 아닐경우가 확실할때
+          //왼쪽 피연산자가 null 또는 undefined일 때만 오른쪽 피연산자 반환
+          return <div style={{ color }}>A1</div>
+        }}
+      </ColorContext.Consumer>
+      <CountContext.Consumer>
+        {({ count }) => <div>{count}</div>}
+      </CountContext.Consumer>
       <A2 />
     </>
   )
