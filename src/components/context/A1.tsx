c22fd8c1d2405999f4fd97eb5a25dda4fc732c9b
KTH8962 <gaya8962@gmail.com>
2025-01-17 22:37:06 +0900

    테스트 커밋

--- src/components/context/A1.tsx
+++ src/components/context/A1.tsx
@@ -7,7 +7,7 @@ function A1() {
       <ColorContext.Consumer>
         {colorState => {
           const [color] = colorState ?? colorState // !는 undefined나 null이 아닐경우가 확실할때
-          //왼쪽 피연산자가 null 또는 undefined일 때만 오른쪽 피연산자 반환
+          //왼쪽 피연산자가 null 또는 undefined일 때만 오른쪽 피연산자 반환한다.
           return <div style={{ color }}>A1</div>
         }}
       </ColorContext.Consumer>
